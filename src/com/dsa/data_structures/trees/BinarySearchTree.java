cc2f2d93edd71982ef1727fbc548d4db81ffea8d
devashish <devashishjaiswal86@gmail.com>
2020-01-03 14:30:44 +0530

    Implementation of tree traversal using the parent node

---
+++ src/com/dsa/data_structures/trees/BinarySearchTree.java
@@ -0,0 +1,158 @@
+package com.dsa.data_structures.trees;
+
+import java.util.Random;
+
+public class BinarySearchTree {
+    private static class Node {
+        Node left;
+        Node right;
+        Node parent;
+        int key;
+
+        Node(int val, Node parent) {
+            this.key = val;
+            this.parent = parent;
+        }
+    }
+
+    private Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int key) {
+        if (root == null) {
+            root = new Node(key, null);
+            return;
+        }
+
+        Node current = root;
+        Node parent = null;
+
+        while (current != null) {
+            parent = current;
+            current = current.key > key ? current.left : current.right;
+        }
+
+        Node newNode = new Node(key, parent);
+        if (parent.key > key)
+            parent.left = newNode;
+        else parent.right = newNode;
+    }
+
+    private void throwExceptionIfTreeIsEmpty() {
+        if (root == null)
+            throw new RuntimeException("Tree is empty!");
+    }
+
+    public void inorderTreeWalk() {
+        if (root == null)
+            return;
+
+        Node current = root;
+        Node visited = null;
+
+        while (root != visited) {
+            if (current == visited) {
+                // if current node is at the right hand side of its parent
+                // mark the parent as visited
+                if (current == current.parent.right)
+                    visited = current.parent;
+
+                current = current.parent;
+                continue;
+            }
+
+            if (current.left != visited && current.left != null) {
+                current = current.left;
+                continue;
+            }
+
+            System.out.println(current.key);
+
+            if (current.right != visited && current.right != null) {
+                current = current.right;
+                continue;
+            }
+
+            visited = current;
+        }
+    }
+
+    public void inorderTreeWalkRecursive(Node root) {
+        if (root != null) {
+            inorderTreeWalkRecursive(root.left);
+            System.out.println(root.key);
+            inorderTreeWalkRecursive(root.right);
+        }
+    }
+
+    public Node inorderSearch(int key) {
+        throwExceptionIfTreeIsEmpty();
+        Node current = root;
+        while (current != null) {
+            if (current.key == key)
+                return current;
+            current = current.key > key ? current.left : current.right;
+        }
+        throw new RuntimeException("Key does not exist!");
+    }
+
+    public void delete(int key) {
+        throwExceptionIfTreeIsEmpty();
+        Node node = inorderSearch(key);
+
+        // fixme: what if we are deleting the root of the tree?
+        Node parent = node.parent;
+        boolean atLeft = parent.left == node;
+
+        if (node.left == null && node.right == null) {
+            // leaf node
+            if (atLeft)
+                parent.left = null;
+            else parent.right = null;
+            return;
+        }
+
+        if (node.left == null) {
+            node.right.parent = parent;
+            if (atLeft)
+                // node is at left and has no left children
+                parent.left = node.right;
+            else
+                // node is at right and has no left children
+                parent.right = node.right;
+        } else if (node.right == null) {
+            node.left.parent = parent;
+            if (atLeft)
+                // node is at left and has no right children
+                parent.left = node.left;
+            else
+                // node is at right and has no right children
+                parent.right = node.left;
+        }
+    }
+
+    private static int[] arrayOfRandoms(int length) {
+        int[] array = new int[length];
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+
+        return array;
+    }
+
+    private static void insertArray(BinarySearchTree tree, int[] array) {
+        for (int value : array)
+            tree.insert(value);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        insertArray(bst, arrayOfRandoms(10000));
+        bst.inorderTreeWalk();
+    }
+}
