2dd04164475d6815be656c8a6f7159f1910cb5dc
devashish <devashishjaiswal86@gmail.com>
2020-01-06 22:19:27 +0530

    Implemented methods to check greater than or equal and less than or equal

--- src/com/dsa/trees/Node.java
+++ src/com/dsa/trees/Node.java
@@ -5,20 +5,46 @@ class Node<T extends Comparable<T>> {
     Node<T> right;
     T key;
 
+    /**
+     * this.key > param.key
+     */
     boolean keyGT(T ob) {
         return key.compareTo(ob) > 0;
     }
 
+    /**
+     * this.key < param.key
+     */
     boolean keyST(T ob) {
         return key.compareTo(ob) < 0;
     }
 
+    /**
+     * this.key == param.key
+     */
     boolean keyEquals(T ob) {
         return key.compareTo(ob) == 0;
     }
 
+    /**
+     * this.key != param.key
+     */
     boolean keyNotEquals(T ob) {
         return !keyEquals(ob);
+    }
+
+    /**
+     * this.key >= param.key
+     */
+    boolean keyGToE(T ob) {
+        return keyGT(ob) || keyEquals(ob);
+    }
+
+    /**
+     * this.key <= param.key
+     */
+    boolean keySToE(T ob) {
+        return keyST(ob) || keyEquals(ob);
     }
 
     Node(T val) {
