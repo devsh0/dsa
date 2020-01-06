865e67d1027f782de4d120274a25c7b74dc5f7a6
devashish <devashishjaiswal86@gmail.com>
2020-01-06 13:23:56 +0530

    Implemented generic node class with comparison methods

--- src/com/dsa/trees/Node.java
+++ src/com/dsa/trees/Node.java
@@ -1,4 +1,28 @@
 package com.dsa.trees;
 
-public class Node {
+class Node<T extends Comparable<T>> {
+    Node<T> left;
+    Node<T> right;
+    T key;
+
+    boolean keyGT(T ob) {
+        return key.compareTo(ob) > 0;
+    }
+
+    boolean keyST(T ob) {
+        return key.compareTo(ob) < 0;
+    }
+
+    boolean keyEquals(T ob) {
+        return key.compareTo(ob) == 0;
+    }
+
+    boolean keyNotEquals(T ob) {
+        return !keyEquals(ob);
+    }
+
+    Node(T val) {
+        this.key = val;
+        //this.parent = parent;
+    }
 }
